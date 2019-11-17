ae6958377f1d79de5f23b646788b8d229b6ae146
Joey Mckenzie <joey32793@gmail.com>
2019-11-17 11:31:42 -0800

    feat(Domain): Add `BreweryId` to `CreateBeerDto`

--- src/Dappery.Domain/Dtos/Beer/CreateBeerDto.cs
+++ src/Dappery.Domain/Dtos/Beer/CreateBeerDto.cs
@@ -1,11 +1,11 @@
 namespace Dappery.Domain.Dtos.Beer
 {
-    using Entities;
-
     public class CreateBeerDto
     {
         public string Name { get; set; }
 
-        public BeerStyle Style { get; set; }
+        public string Style { get; set; }
+
+        public int BreweryId { get; set; }
     }
 }
